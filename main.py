3166d292bd7b610d82ada41a3fd0e6f5de9e5da4
CyberFlame <cyberflameu@gmail.com>
2020-10-21 14:43:26 +1300

    add py and ts and set ts/js to main editor

--- main.py
+++ main.py
@@ -1,63 +1,59 @@
-let playerA = 0
-let playerB = 0
-basic.forever(function on_forever() {
-    
+playerA = 0
+playerB = 0
+
+def on_forever():
+    global playerA, playerB
     basic.pause(1000)
     basic.pause(randint(0, 4000))
-    basic.showLeds(`
+    basic.show_leds("""
         . . . . .
         . # # # .
         . # # # .
         . # # # .
         . . . . .
-        `)
-    while (!input.buttonIsPressed(Button.A) && !input.buttonIsPressed(Button.B)) {
+        """)
+    while not (input.button_is_pressed(Button.A)) and not (input.button_is_pressed(Button.B)):
         basic.pause(20)
-    }
-    if (input.buttonIsPressed(Button.A)) {
-        basic.showLeds(`
+    if input.button_is_pressed(Button.A):
+        basic.show_leds("""
             . . # . .
             . # . . .
             # # # # #
             . # . . .
             . . # . .
-            `)
+            """)
         playerA += 1
-    }
-    
-    if (input.buttonIsPressed(Button.B)) {
-        basic.showLeds(`
+    if input.button_is_pressed(Button.B):
+        basic.show_leds("""
             . . # . .
             . . . # .
             # # # # #
             . . . # .
             . . # . .
-            `)
+            """)
         playerB += 1
-    }
-    
     basic.pause(1000)
-    basic.clearScreen()
+    basic.clear_screen()
     basic.pause(1000)
-    basic.showLeds(`
+    basic.show_leds("""
         . . # . .
         . # . . .
         # # # # #
         . # . . .
         . . # . .
-        `)
+        """)
     basic.pause(1000)
-    basic.showNumber(playerA)
+    basic.show_number(playerA)
     basic.pause(1000)
-    basic.showLeds(`
+    basic.show_leds("""
         . . # . .
         . . . # .
         # # # # #
         . . . # .
         . . # . .
-        `)
+        """)
     basic.pause(1000)
-    basic.showNumber(playerB)
+    basic.show_number(playerB)
     basic.pause(1000)
-    basic.clearScreen()
-})
+    basic.clear_screen()
+basic.forever(on_forever)
